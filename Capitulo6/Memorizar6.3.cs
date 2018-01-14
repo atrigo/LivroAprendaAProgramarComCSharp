a530908ed3fa8b867e336e601494d4797414e2de
Antonio Trigo <antonio.trigo@gmail.com>
2018-01-14 18:14:09 +0000

    Update Memorizar6.3.cs

--- Capitulo6/Memorizar6.3.cs
+++ Capitulo6/Memorizar6.3.cs
@@ -4,3 +4,26 @@
  * Autores: Antonio Trigo e Jorge Henriques
  * Disponível em: http://www.silabo.pt
 */
+
+using System;
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero, menor;
+            Console.WriteLine("Insira um numero:");
+            numero = Convert.ToInt32(Console.ReadLine());
+            menor = numero;
+            while (numero != 0)
+            {
+                Console.WriteLine("Insira um numero:");
+                numero = Convert.ToInt32(Console.ReadLine());
+                if (menor > numero && numero != 0)
+                    menor = numero;
+            }
+            Console.WriteLine("O menor numero e {0}.", menor);
+        }
+    }
+}
