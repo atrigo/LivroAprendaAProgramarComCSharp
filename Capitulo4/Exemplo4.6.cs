4f6a2623408034aa4391b7271df54110204903c1
Antonio Trigo <antonio.trigo@gmail.com>
2018-01-14 16:29:13 +0000

    Update Exemplo4.6.cs

--- Capitulo4/Exemplo4.6.cs
+++ Capitulo4/Exemplo4.6.cs
@@ -4,3 +4,17 @@
  * Autores: Antonio Trigo e Jorge Henriques
  * Disponível em: http://www.silabo.pt
 */
+
+using System;
+namespace ConsoleApp1
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Insira uma letra e carregue Enter");
+            Console.WriteLine(Console.Read()); //Mostra o codigo ASCII da letra
+            Console.WriteLine(Console.Read()); //Mostra o codigo ASCII do Enter
+        }
+    }
+}
