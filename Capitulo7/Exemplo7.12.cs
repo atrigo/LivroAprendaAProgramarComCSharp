6b439b41f89b6a410c330438e3a9435f896756f7
Antonio Trigo <antonio.trigo@gmail.com>
2018-01-14 18:34:44 +0000

    Update Exemplo7.12.cs

--- Capitulo7/Exemplo7.12.cs
+++ Capitulo7/Exemplo7.12.cs
@@ -4,3 +4,24 @@
  * Autores: Antonio Trigo e Jorge Henriques
  * Disponível em: http://www.silabo.pt
 */
+
+using System;
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static int Fatorial(int n)
+        {
+            if (n == 1) //Criterio de paragem
+                return n;
+            else
+                return n * Fatorial(n - 1);
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("Insira o numero cujo fatorial deseja calcular:");
+            int n = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("{0}!={1}", n, Fatorial(n));
+        }
+    }
+}
