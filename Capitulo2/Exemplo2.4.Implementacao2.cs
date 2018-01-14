55633d94b2c41b1082e21a2a9d82b585e32ad53c
Antonio Trigo <antonio.trigo@gmail.com>
2018-01-14 15:13:06 +0000

    Create Exemplo2.4.Implementacao2.cs

---
+++ Capitulo2/Exemplo2.4.Implementacao2.cs
@@ -0,0 +1,12 @@
+using System;
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 10; i++)
+                Console.WriteLine(" Antonio Trigo ");
+        }
+    }
+}
