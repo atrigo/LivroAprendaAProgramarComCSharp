43d00ce271c079854a9c0b87b856e52f1ec5d4ae
Antonio Trigo <antonio.trigo@gmail.com>
2018-01-14 18:59:19 +0000

    Update Exemplo9.1.cs

--- Capitulo9/Exemplo9.1.cs
+++ Capitulo9/Exemplo9.1.cs
@@ -4,3 +4,21 @@
  * Autores: Antonio Trigo e Jorge Henriques
  * Disponível em: http://www.silabo.pt
 */
+
+using System;
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string frase = "Bom dia!";
+            //Diretamente
+            Console.WriteLine(frase);
+            //Utilizando um ciclo for
+            for (int i = 0; i < frase.Length; i++)
+                Console.Write("{0}", frase[i]);
+            Console.WriteLine();
+        }
+    }
+}
