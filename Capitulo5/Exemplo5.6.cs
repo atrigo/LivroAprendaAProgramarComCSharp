649cd0b992afb17df5ab482f7a860471f0eef835
Antonio Trigo <antonio.trigo@gmail.com>
2018-01-14 15:27:09 +0000

    Update and rename Exemplo_56 to Exemplo5.6.cs

--- Capitulo5/Exemplo5.6.cs
+++ Capitulo5/Exemplo5.6.cs
@@ -1,9 +1,10 @@
 /*
- * Exemplo: Máquina de calcular
- * Autor: Antonio Trigo
- * Livro: Introdução à programação em C#
- * Disponível em: 
+ * Exemplo 2.2
+ * Livro: Aprenda a programar com C#
+ * Autores: Antonio Trigo e Jorge Henriques
+ * Disponível em: http://www.silabo.pt
 */
+
 
 using System;
 namespace ConsoleApp1
