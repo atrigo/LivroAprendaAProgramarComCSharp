fd21123856553acc2a68f9015c98be62d101f749
Antonio Trigo <antonio.trigo@gmail.com>
2018-01-14 18:04:12 +0000

    Update Exemplo6.3.cs

--- Capitulo6/Exemplo6.3.cs
+++ Capitulo6/Exemplo6.3.cs
@@ -4,3 +4,19 @@
  * Autores: Antonio Trigo e Jorge Henriques
  * Disponível em: http://www.silabo.pt
 */
+
+using System;
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i;
+            for (i = 1; i <= 10; i++)
+            {
+                Console.WriteLine("5 x {0} = {1}", i, 5 * i);
+            }
+        }
+    }
+}
