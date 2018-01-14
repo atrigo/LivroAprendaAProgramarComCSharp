dcfa47f6c4d9218cfad63a1b6a41e90c4494d8e4
Antonio Trigo <antonio.trigo@gmail.com>
2018-01-14 16:14:04 +0000

    Update Exemplo3.1.cs

--- Capitulo3/Exemplo3.1.cs
+++ Capitulo3/Exemplo3.1.cs
@@ -4,3 +4,24 @@
  * Autores: Antonio Trigo e Jorge Henriques
  * Disponível em: http://www.silabo.pt
 */
+
+using System;
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            short a = 5;
+            int b;
+            float c = 8.5f;
+            b = a;
+            // No conversao acima nao ha perda de informacao
+            Console.WriteLine("Inteiro: " + b);
+            Console.WriteLine("Real: " + c);
+            b = (int)c;
+            // Na conversao acima perde -se a informacao do valor decimal
+        Console.WriteLine("Inteiro: " + b);
+        }
+    }
+}
