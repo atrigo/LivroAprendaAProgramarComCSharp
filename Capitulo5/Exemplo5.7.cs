0ab257a23658b8590cc9345e0148915564a0db95
Antonio Trigo <antonio.trigo@gmail.com>
2018-01-14 15:28:11 +0000

    Update and rename Memorizar_maiordedoisvalores to Exemplo5.7.cs

--- Capitulo5/Exemplo5.7.cs
+++ Capitulo5/Exemplo5.7.cs
@@ -1,3 +1,11 @@
+/*
+ * Exemplo 5.7
+ * Livro: Aprenda a programar com C#
+ * Autores: Antonio Trigo e Jorge Henriques
+ * Disponível em: http://www.silabo.pt
+*/
+
+
 using System;
 namespace ConsoleApp1{
     class Program{
