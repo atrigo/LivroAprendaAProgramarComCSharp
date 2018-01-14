b3b70f16149919f86f8c44cc2ade75e3836de223
Antonio Trigo <antonio.trigo@gmail.com>
2018-01-14 16:41:34 +0000

    Update Exemplo5.11.cs

--- Capitulo5/Exemplo5.11.cs
+++ Capitulo5/Exemplo5.11.cs
@@ -1,1 +1,16 @@
 
+
+using System;
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int valor1;
+            Console.WriteLine("Insira um numero: ");
+            valor1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("{0}", valor1 % 2 == 0 ? "par":"impar");
+        }
+    }
+}
