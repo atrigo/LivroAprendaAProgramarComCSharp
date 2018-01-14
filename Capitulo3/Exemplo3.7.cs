4cc9a109abf95e26b6c2425f591a04a3c818d77b
Antonio Trigo <antonio.trigo@gmail.com>
2018-01-14 16:22:58 +0000

    Update Exemplo3.7.cs

--- Capitulo3/Exemplo3.7.cs
+++ Capitulo3/Exemplo3.7.cs
@@ -4,3 +4,21 @@
  * Autores: Antonio Trigo e Jorge Henriques
  * Disponível em: http://www.silabo.pt
 */
+
+using System;
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 10;
+            Console.WriteLine("{0}", a = 5);
+            Console.WriteLine("{0}", a += 3);
+            Console.WriteLine("{0}", a -= 2);
+            Console.WriteLine("{0}", a *= 5);
+            Console.WriteLine("{0}", a /= 2);
+            Console.WriteLine("{0}", a %= 4);
+        }
+    }
+}
