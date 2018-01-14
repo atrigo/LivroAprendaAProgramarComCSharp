a3c0580e04288d5c1aa38d14eec2ffc78a487825
Antonio Trigo <antonio.trigo@gmail.com>
2018-01-14 16:15:18 +0000

    Update Exemplo3.2.cs

--- Capitulo3/Exemplo3.2.cs
+++ Capitulo3/Exemplo3.2.cs
@@ -4,3 +4,23 @@
  * Autores: Antonio Trigo e Jorge Henriques
  * Disponível em: http://www.silabo.pt
 */
+
+
+using System;
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nome;
+            int idade;
+            Console.Write(" Insira o seu nome : ");
+            nome = Console.ReadLine();
+            Console.Write(" Insira a sua idade : ");
+            idade = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(" Bem vindo {0}. Tem {1} ano (s).",
+            nome, idade);
+        }
+    }
+}
