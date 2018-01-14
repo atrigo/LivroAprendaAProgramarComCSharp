f2b0f41cb0aa447b5c95dae9bde11f74bf7158be
Antonio Trigo <antonio.trigo@gmail.com>
2018-01-14 19:06:42 +0000

    Update Exemplo9.9.cs

--- Capitulo9/Exemplo9.9.cs
+++ Capitulo9/Exemplo9.9.cs
@@ -4,3 +4,21 @@
  * Autores: Antonio Trigo e Jorge Henriques
  * Disponível em: http://www.silabo.pt
 */
+
+using System;
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Insira uma hora no formato HH:mm:ss: ");
+            string hora = Console.ReadLine();
+            string[] argTempo = hora.Split(':');
+            int horas = Convert.ToInt32(argTempo[0]);
+            int minutos = Convert.ToInt32(argTempo[1]);
+            int segundos = Convert.ToInt32(argTempo[2]);
+            Console.WriteLine("Ja passaram {0} segundos desde o inicio do dia.", horas * 60 * 60 + minutos * 60 + segundos);
+        }
+    }
+}
