84e09ed767c6b995529410dca2c9e94771fb5993
Antonio Trigo <antonio.trigo@gmail.com>
2018-01-14 19:01:57 +0000

    Update Exemplo9.4.cs

--- Capitulo9/Exemplo9.4.cs
+++ Capitulo9/Exemplo9.4.cs
@@ -4,3 +4,19 @@
  * Autores: Antonio Trigo e Jorge Henriques
  * Disponível em: http://www.silabo.pt
 */
+
+using System;
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string strA = "A";
+            string strB = "B";
+            Console.WriteLine(strA.CompareTo(strB)); //-1
+            Console.WriteLine(strB.CompareTo(strA)); // 1
+            Console.WriteLine(strB.CompareTo(strB)); // 0
+        }
+    }
+}
