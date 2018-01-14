f02ba00567dabfde07b76c412d9db42298a9d9fa
Antonio Trigo <antonio.trigo@gmail.com>
2018-01-14 16:28:34 +0000

    Update Exemplo4.5.cs

--- Capitulo4/Exemplo4.5.cs
+++ Capitulo4/Exemplo4.5.cs
@@ -4,3 +4,17 @@
  * Autores: Antonio Trigo e Jorge Henriques
  * Disponível em: http://www.silabo.pt
 */
+
+using System;
+namespace ConsoleApp1
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Como se chama?");
+            string nome = Console.ReadLine();
+            Console.WriteLine("Bem vindo {0}", nome);
+        }
+    }
+}
