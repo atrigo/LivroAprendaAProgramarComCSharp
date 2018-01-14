b42576690730a3fcf6a4544c20ad7328a8eb2354
Antonio Trigo <antonio.trigo@gmail.com>
2018-01-14 16:33:37 +0000

    Update Exemplo5.5.cs

--- Capitulo5/Exemplo5.5.cs
+++ Capitulo5/Exemplo5.5.cs
@@ -5,7 +5,6 @@
  * Disponível em: http://www.silabo.pt
 */
 
-
 using System;
 namespace ConsoleApp1
 {
@@ -13,25 +12,11 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            float peso, altura, imc;
-            Console.WriteLine("Insira um valor para o peso: ");
-            peso = Convert.ToSingle(Console.ReadLine());
-            Console.WriteLine("Insira um valor para a altura (em metros ex.: 1,76): ");
-            altura = Convert.ToSingle(Console.ReadLine());
-            imc = peso / (altura * altura);
-            if (imc < 18.5)
-                Console.WriteLine("Magreza");
-            else
-                if (imc >= 18.5 && imc < 25)
-                    Console.WriteLine("Normoponderal");
-                else
-                    if (imc >= 25 && imc < 30)
-                        Console.WriteLine("Pre-obesidade");
-                    else
-                        if (imc >= 30 && imc < 40)
-                            Console.WriteLine("Obesidade");
-                        else
-                            Console.WriteLine("Obesidade morbida");
+            float nota;
+            Console.Write("Insira a nota que obteve no exame: ");
+            nota = Convert.ToSingle(Console.ReadLine());
+            if (nota >= 9.5)
+                Console.WriteLine("Aprovado.");
         }
     }
 }
