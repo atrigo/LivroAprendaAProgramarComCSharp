8e87c6a46e053dd9f1a7452dbfc12b945c477d9d
Antonio Trigo <antonio.trigo@gmail.com>
2018-01-14 18:31:41 +0000

    Update Exemplo7.7.cs

--- Capitulo7/Exemplo7.7.cs
+++ Capitulo7/Exemplo7.7.cs
@@ -4,3 +4,34 @@
  * Autores: Antonio Trigo e Jorge Henriques
  * Disponível em: http://www.silabo.pt
 */
+
+using System;
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void TrocaValor(int x, int y)
+        {
+            int aux;
+            aux = x;
+            x = y;
+            y = aux;
+        }
+        static void TrocaReferencia(ref int x, ref int y)
+        {
+            int aux;
+            aux = x;
+            x = y;
+            y = aux;
+        }
+        static void Main(string[] args)
+        {
+            int a = 5, b = 7;
+            Console.WriteLine("O valor inicial das variaveis e a:{0} e b:{1}", a, b);
+            TrocaValor(a, b);
+            Console.WriteLine("O valor das variaveis depois de chamada a funcao TrocaValor e a:{0} e b:{1}", a, b);
+            TrocaReferencia(ref a, ref b);
+            Console.WriteLine("O valor das variaveis depois de chamada a funcao TrocaReferencia e a:{0} e b:{1}", a, b);
+        }
+    }
+}
