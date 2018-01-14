f7cd4498fd12af04ebb607b76da96e468577ccdc
Antonio Trigo <antonio.trigo@gmail.com>
2018-01-14 18:41:46 +0000

    Update Memorizar8.1.cs

--- Capitulo8/Memorizar8.1.cs
+++ Capitulo8/Memorizar8.1.cs
@@ -4,3 +4,23 @@
  * Autores: Antonio Trigo e Jorge Henriques
  * Disponível em: http://www.silabo.pt
 */
+
+using System;
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] v = new int[] { 3, 7, 4, 9, 2, 6, 7, 8, 9, 2 };
+            for (int i = 0; i < v.Length; i++)
+            {
+                if (v[i] == 7)
+                {
+                    Console.WriteLine("Indice: {0}", i);
+                    break;
+                }
+            }
+        }
+    }
+}
