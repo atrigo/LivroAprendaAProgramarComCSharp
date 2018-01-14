fa511aace2d73593305cf86cc3b54ecb79cab2ba
Antonio Trigo <antonio.trigo@gmail.com>
2018-01-14 18:03:34 +0000

    Update Exemplo6.2.cs

--- Capitulo6/Exemplo6.2.cs
+++ Capitulo6/Exemplo6.2.cs
@@ -4,3 +4,20 @@
  * Autores: Antonio Trigo e Jorge Henriques
  * Disponível em: http://www.silabo.pt
 */
+
+using System;
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int soma = 0;
+            for (int i = 1; i < 51; i++)
+            {
+                soma = soma + i; //soma += i;
+            }
+            Console.WriteLine(soma);
+        }
+    }
+}
