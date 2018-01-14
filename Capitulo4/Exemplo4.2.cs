9afb2d542a12cd8d90c0425319aaf8662ba17352
Antonio Trigo <antonio.trigo@gmail.com>
2018-01-14 16:25:37 +0000

    Update Exemplo4.2.cs

--- Capitulo4/Exemplo4.2.cs
+++ Capitulo4/Exemplo4.2.cs
@@ -4,3 +4,18 @@
  * Autores: Antonio Trigo e Jorge Henriques
  * Disponível em: http://www.silabo.pt
 */
+
+using System;
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(5);
+            Console.WriteLine(5 + 6);
+            Console.WriteLine(5 * 6);
+            Console.WriteLine("O andre tem " + 5 + " anos");
+        }
+    }
+}
