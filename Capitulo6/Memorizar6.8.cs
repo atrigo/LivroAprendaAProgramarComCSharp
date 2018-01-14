ccc8431a4c8e020f7e1e2e06ea078bead1c7c98b
Antonio Trigo <antonio.trigo@gmail.com>
2018-01-14 18:23:01 +0000

    Update Memorizar6.8.cs

--- Capitulo6/Memorizar6.8.cs
+++ Capitulo6/Memorizar6.8.cs
@@ -4,3 +4,22 @@
  * Autores: Antonio Trigo e Jorge Henriques
  * Disponível em: http://www.silabo.pt
 */
+
+using System;
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int idade;
+            do
+            {
+                Console.Write("Insira uma idade entre 1 e 120 anos: ");
+                idade = Convert.ToInt32(Console.ReadLine());
+                if (idade < 1 || idade > 120)
+                    Console.WriteLine("Idade incorreta.");
+            } while (idade < 1 || idade > 120);
+        }
+    }
+}
