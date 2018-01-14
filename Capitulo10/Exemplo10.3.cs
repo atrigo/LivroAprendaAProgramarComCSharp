76cbee1ab19e9a4c1ad5ea7f0f4656c0ff5df284
Antonio Trigo <antonio.trigo@gmail.com>
2018-01-14 19:09:43 +0000

    Update Exemplo10.3.cs

--- Capitulo10/Exemplo10.3.cs
+++ Capitulo10/Exemplo10.3.cs
@@ -4,3 +4,24 @@
  * Autores: Antonio Trigo e Jorge Henriques
  * Disponível em: http://www.silabo.pt
 */
+
+using System;
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero1 = 3000;
+            int numero2 = 0;
+            try
+            {
+                Console.WriteLine(numero1 / numero2);
+            }
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine("Divisao {0} por zero.", numero1);
+            }
+        }
+    }
+}
