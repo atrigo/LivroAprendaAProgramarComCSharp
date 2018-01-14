3a88e962fe2912f78a9e8d55f503ce2e6211506f
Antonio Trigo <antonio.trigo@gmail.com>
2018-01-14 16:23:40 +0000

    Update Exemplo3.8.cs

--- Capitulo3/Exemplo3.8.cs
+++ Capitulo3/Exemplo3.8.cs
@@ -4,3 +4,21 @@
  * Autores: Antonio Trigo e Jorge Henriques
  * Disponível em: http://www.silabo.pt
 */
+
+
+using System;
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1, num2;
+            Console.WriteLine("Insira um numero: ");
+            num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Insira outro numero: ");
+            num2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("{0}", num1 + num2);
+        }
+    }
+}
