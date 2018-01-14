5a0683f5164221e268845a6ba63933386c68d396
Antonio Trigo <antonio.trigo@gmail.com>
2018-01-14 19:01:26 +0000

    Update Exemplo9.3.cs

--- Capitulo9/Exemplo9.3.cs
+++ Capitulo9/Exemplo9.3.cs
@@ -4,3 +4,23 @@
  * Autores: Antonio Trigo e Jorge Henriques
  * Disponível em: http://www.silabo.pt
 */
+
+using System;
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string frase1 = "C#";
+            string frase2 = "C#";
+            string frase3 = "c#";
+            Console.WriteLine(frase1.Equals("C#")); //True
+            Console.WriteLine(frase1.Equals(frase2)); //True
+            Console.WriteLine(frase1.Equals(frase3)); //False
+            Console.WriteLine(frase1 == "C#"); //True
+            Console.WriteLine(frase1 == frase2); //True
+            Console.WriteLine(frase1 == frase3); //False
+        }
+    }
+}
