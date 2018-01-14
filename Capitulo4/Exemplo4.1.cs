3a2113dc78c5f84f56bbc48543ca01a1cbf71f7d
Antonio Trigo <antonio.trigo@gmail.com>
2018-01-14 16:25:05 +0000

    Update Exemplo4.1.cs

--- Capitulo4/Exemplo4.1.cs
+++ Capitulo4/Exemplo4.1.cs
@@ -4,3 +4,17 @@
  * Autores: Antonio Trigo e Jorge Henriques
  * Disponível em: http://www.silabo.pt
 */
+
+using System;
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Bom ");
+            Console.WriteLine("dia!");
+            Console.WriteLine("Jorge.");
+        }
+    }
+}
