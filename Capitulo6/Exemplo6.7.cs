6bc16549abe5e3ce0899c1eec39e2c8268b66945
Antonio Trigo <antonio.trigo@gmail.com>
2018-01-14 18:10:32 +0000

    Update Exemplo6.7.cs

--- Capitulo6/Exemplo6.7.cs
+++ Capitulo6/Exemplo6.7.cs
@@ -4,3 +4,26 @@
  * Autores: Antonio Trigo e Jorge Henriques
  * Disponível em: http://www.silabo.pt
 */
+
+using System;
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int asteriscos, i, j;
+            Console.Write("Quantos asteriscos deseja para o quadrado?");
+            asteriscos = Convert.ToInt32(Console.ReadLine());
+            for (i = 0; i < asteriscos; i++)
+            {
+                for (j = 0; j < asteriscos; j++)
+                    if (i == 0 || i == asteriscos - 1 || j == 0 || j == asteriscos - 1)
+                        Console.Write('*');
+                    else
+                        Console.Write(' ');
+                Console.WriteLine();
+            }
+        }
+    }
+}
