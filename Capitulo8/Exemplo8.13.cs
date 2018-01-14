0cd25be8f224c0fb026656820676875de0a9e7fa
Antonio Trigo <antonio.trigo@gmail.com>
2018-01-14 18:57:08 +0000

    Update Exemplo8.13.cs

--- Capitulo8/Exemplo8.13.cs
+++ Capitulo8/Exemplo8.13.cs
@@ -4,3 +4,28 @@
  * Autores: Antonio Trigo e Jorge Henriques
  * Disponível em: http://www.silabo.pt
 */
+
+
+using System;
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void ApresentarVetor(int[] v)
+        {
+            Console.Write("Vetor: ");
+            foreach (int elemento in v)
+                Console.Write("{0} ", elemento);
+            Console.WriteLine();
+        }
+        static void Main()
+        {
+            int[] arr = { 1, 4, 5 };
+            ApresentarVetor(arr);
+            Array.Resize(ref arr, 2);
+            ApresentarVetor(arr);
+            Array.Resize(ref arr, 6);
+            ApresentarVetor(arr);
+        }
+    }
+}
