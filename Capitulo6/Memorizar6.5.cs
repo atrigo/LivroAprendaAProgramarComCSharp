0b12924cf515480b5b094d7b4e213c6896a09e32
Antonio Trigo <antonio.trigo@gmail.com>
2018-01-14 18:17:46 +0000

    Update Memorizar6.5.cs

--- Capitulo6/Memorizar6.5.cs
+++ Capitulo6/Memorizar6.5.cs
@@ -4,3 +4,23 @@
  * Autores: Antonio Trigo e Jorge Henriques
  * Disponível em: http://www.silabo.pt
 */
+
+using System;
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero, contar = 0;
+            do
+            {
+                Console.WriteLine("Insira um numero:");
+                numero = Convert.ToInt32(Console.ReadLine());
+                if (numero != 0)
+                    contar++;
+            } while (numero != 0);
+            Console.WriteLine("Foram inseridos {0} numeros.", contar);
+        }
+    }
+}
