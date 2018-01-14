e4b9c252640b405af8f4398866294c85c8d1c6c9
Antonio Trigo <antonio.trigo@gmail.com>
2018-01-14 18:28:58 +0000

    Update Exemplo7.3.cs

--- Capitulo7/Exemplo7.3.cs
+++ Capitulo7/Exemplo7.3.cs
@@ -4,3 +4,24 @@
  * Autores: Antonio Trigo e Jorge Henriques
  * Disponível em: http://www.silabo.pt
 */
+
+using System;
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void IntFrac(float x)
+        {
+            Console.Write("Parte inteira: {0} \n", (int)x);
+            Console.Write("Parte fracionaria: {0:F3}\n", x - (int)x);
+        }
+
+        static void Main(string[] args)
+        {
+            float num;
+            Console.Write("Escreva um numero real: ");
+            num = Convert.ToSingle(Console.ReadLine());
+            IntFrac(num);
+        }
+    }
+}
