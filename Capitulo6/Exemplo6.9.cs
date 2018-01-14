e9ec6be07ff8177d6a48be98c993fec4ca31f638
Antonio Trigo <antonio.trigo@gmail.com>
2018-01-14 18:16:48 +0000

    Update Exemplo6.9.cs

--- Capitulo6/Exemplo6.9.cs
+++ Capitulo6/Exemplo6.9.cs
@@ -4,3 +4,25 @@
  * Autores: Antonio Trigo e Jorge Henriques
  * Disponível em: http://www.silabo.pt
 */
+
+using System;
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero, aux, digito, soma = 0;
+            Console.WriteLine("Insira um numero:");
+            numero = Convert.ToInt32(Console.ReadLine());
+            aux = numero;
+            while (aux != 0)
+            {
+                digito = aux % 10;
+                aux /= 10;
+                soma += digito;
+            }
+            Console.WriteLine("A soma dos digitos do numero {0} e {1}.", numero, soma);
+        }
+    }
+}
