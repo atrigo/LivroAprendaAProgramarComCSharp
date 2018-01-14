8f55057fcd32cdf51cfd7767fccb914be0bc6084
Antonio Trigo <antonio.trigo@gmail.com>
2018-01-14 16:18:54 +0000

    Update Exemplo3.3.cs

--- Capitulo3/Exemplo3.3.cs
+++ Capitulo3/Exemplo3.3.cs
@@ -4,3 +4,20 @@
  * Autores: Antonio Trigo e Jorge Henriques
  * Disponível em: http://www.silabo.pt
 */
+
+using System;
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double raio;
+            const double pi = 3.14;
+            Console.WriteLine("Insira um valor para o raio : ");
+            raio = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("O valor da area do circulo de raio {0} e: {1}", raio , 2 * pi * raio * raio );
+            Console.WriteLine("O valor da area do circulo de raio {0} e: {1}", raio , 2* Math .PI* raio * raio );
+        }
+    }
+}
