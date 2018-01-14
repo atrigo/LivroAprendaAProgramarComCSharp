fdbfe4eb0f7348802a196bdbfe5ed8c69a0ffe12
Antonio Trigo <antonio.trigo@gmail.com>
2018-01-14 18:34:10 +0000

    Update Exemplo7.11.cs

--- Capitulo7/Exemplo7.11.cs
+++ Capitulo7/Exemplo7.11.cs
@@ -4,3 +4,24 @@
  * Autores: Antonio Trigo e Jorge Henriques
  * Disponível em: http://www.silabo.pt
 */
+
+using System;
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static int Soma(int n)
+        {
+            if (n == 0) //Criterio de paragem
+                return n;
+            else
+                return n + Soma(n - 1);
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("Deseja fazer a soma de quantos numeros:");
+            int n = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Soma: {0}", Soma(n));
+        }
+    }
+}
