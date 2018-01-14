17ccbba311ae8dac90054551a309878442a74e91
Antonio Trigo <antonio.trigo@gmail.com>
2018-01-14 18:42:11 +0000

    Update Memorizar8.2.cs

--- Capitulo8/Memorizar8.2.cs
+++ Capitulo8/Memorizar8.2.cs
@@ -4,3 +4,21 @@
  * Autores: Antonio Trigo e Jorge Henriques
  * Disponível em: http://www.silabo.pt
 */
+
+using System;
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] v = new int[] { 3, 7, 4, 9, 2, 6, 7, 8, 9, 2 };
+            int soma = 0;
+            for (int i = 0; i < v.Length; i++)
+            {
+                soma += v[i];
+            }
+            Console.WriteLine("Resultado: {0}", soma);
+        }
+    }
+}
