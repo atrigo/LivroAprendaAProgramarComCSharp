ac0bad59d705d57f297d397ff1b2b0ce3dc84c1c
Antonio Trigo <antonio.trigo@gmail.com>
2018-01-14 19:02:46 +0000

    Update Exemplo9.5.cs

--- Capitulo9/Exemplo9.5.cs
+++ Capitulo9/Exemplo9.5.cs
@@ -4,3 +4,25 @@
  * Autores: Antonio Trigo e Jorge Henriques
  * Disponível em: http://www.silabo.pt
 */
+
+using System;
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string frase;
+            int i = 0;
+
+            Console.Write("Insira uma frase: ");
+            frase = Console.ReadLine();
+            while (frase[i] != ' ')
+            {
+                Console.Write(frase[i]);
+                i++;
+            }
+            Console.WriteLine();
+        }
+    }
+}
