d4be12c8c66a0dc91155f4b902db613f158e368e
Antonio Trigo <antonio.trigo@gmail.com>
2018-01-14 16:37:25 +0000

    Update Exemplo5.9.cs

--- Capitulo5/Exemplo5.9.cs
+++ Capitulo5/Exemplo5.9.cs
@@ -4,3 +4,23 @@
  * Autores: Antonio Trigo e Jorge Henriques
  * Disponível em: http://www.silabo.pt
 */
+
+using System;
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int lado1, lado2;
+            Console.Write("Insira um lado: ");
+            lado1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Insira outro lado: ");
+            lado2 = Convert.ToInt32(Console.ReadLine());
+            if (lado1 == lado2)
+                Console.WriteLine("A figura geometrica e um quadrado.");
+            else
+                Console.WriteLine("A figura geometrica e um retangulo.");
+        }
+    }
+}
