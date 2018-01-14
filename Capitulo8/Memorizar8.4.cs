67191765252596bd797339b236f89ba1fbb564da
Antonio Trigo <antonio.trigo@gmail.com>
2018-01-14 18:43:19 +0000

    Update Memorizar8.4.cs

--- Capitulo8/Memorizar8.4.cs
+++ Capitulo8/Memorizar8.4.cs
@@ -4,3 +4,22 @@
  * Autores: Antonio Trigo e Jorge Henriques
  * Disponível em: http://www.silabo.pt
 */
+
+using System;
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] v = new int[] { 3, 7, 4, 9, 2, 6, 7, 8, 9, 2 };
+            int menor = v[0];
+            for (int i = 0; i < v.Length; i++)
+            {
+                if (v[i] < menor)
+                    menor = v[i];
+            }
+            Console.WriteLine("Resultado: {0}", menor);
+        }
+    }
+}
