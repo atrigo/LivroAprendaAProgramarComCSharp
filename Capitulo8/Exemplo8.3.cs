f45ffb3140decc829a9429157db7ba167465516a
Antonio Trigo <antonio.trigo@gmail.com>
2018-01-14 18:40:31 +0000

    Update Exemplo8.3.cs

--- Capitulo8/Exemplo8.3.cs
+++ Capitulo8/Exemplo8.3.cs
@@ -4,3 +4,22 @@
  * Autores: Antonio Trigo e Jorge Henriques
  * Disponível em: http://www.silabo.pt
 */
+
+using System;
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] v = new int[] { 3, 7, 4, 9, 2, 6, 7, 8, 9, 2 };
+            int contar = 0;
+            for (int i = 0; i < v.Length; i++)
+            {
+                if (v[i] > 5)
+                    contar++;
+            }
+            Console.WriteLine("Resultado: {0}", contar);
+        }
+    }
+}
