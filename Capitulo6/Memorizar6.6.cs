5ef5f50fd0ee432fc4a81fc6cbf41fe318df1fce
Antonio Trigo <antonio.trigo@gmail.com>
2018-01-14 18:20:19 +0000

    Update Memorizar6.6.cs

--- Capitulo6/Memorizar6.6.cs
+++ Capitulo6/Memorizar6.6.cs
@@ -4,3 +4,23 @@
  * Autores: Antonio Trigo e Jorge Henriques
  * Disponível em: http://www.silabo.pt
 */
+
+using System;
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero, somar = 0;
+            do
+            {
+                Console.WriteLine("Insira um numero:");
+                numero = Convert.ToInt32(Console.ReadLine());
+                if (numero != 0)
+                    somar += numero;
+            } while (numero != 0);
+            Console.WriteLine("A soma dos numeros e {0}.", somar);
+        }
+    }
+}
