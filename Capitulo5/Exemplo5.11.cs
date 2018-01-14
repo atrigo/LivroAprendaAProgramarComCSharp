219f5a7a53e40811c5f3eacd9fd69d62c70a60d6
Antonio Trigo <antonio.trigo@gmail.com>
2018-01-14 16:41:59 +0000

    Update Exemplo5.11.cs

--- Capitulo5/Exemplo5.11.cs
+++ Capitulo5/Exemplo5.11.cs
@@ -1,4 +1,9 @@
-
+/*
+ * Exemplo 5.11
+ * Livro: Aprenda a programar com C#
+ * Autores: Antonio Trigo e Jorge Henriques
+ * Disponível em: http://www.silabo.pt
+*/
 
 using System;
 namespace ConsoleApp1
