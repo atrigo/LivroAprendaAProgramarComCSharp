6698d98ae85d8c69eff26e183c50ce9e167856aa
Antonio Trigo <antonio.trigo@gmail.com>
2018-01-14 19:00:05 +0000

    Update Exemplo9.2.cs

--- Capitulo9/Exemplo9.2.cs
+++ Capitulo9/Exemplo9.2.cs
@@ -5,3 +5,17 @@
  * Autores: Antonio Trigo e Jorge Henriques
  * Disponível em: http://www.silabo.pt
 */
+
+using System;
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string frase = "B";
+            frase = frase + "om dia!";
+            Console.WriteLine(frase);
+        }
+    }
+}
