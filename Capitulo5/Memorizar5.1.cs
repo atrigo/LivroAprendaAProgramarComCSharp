ccf0848fc3c27232900a9d1693aead8a1d0f057e
Antonio Trigo <antonio.trigo@gmail.com>
2018-01-14 16:36:38 +0000

    Update Memorizar5.1.cs

--- Capitulo5/Memorizar5.1.cs
+++ Capitulo5/Memorizar5.1.cs
@@ -4,3 +4,21 @@
  * Autores: Antonio Trigo e Jorge Henriques
  * Disponível em: http://www.silabo.pt
 */
+
+using System;
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+            Console.Write("Insira um numero: ");
+            num = Convert.ToInt32(Console.ReadLine());
+            if ((num % 2) == 0)
+                Console.WriteLine("O " + num + " e par.");
+            else
+                Console.WriteLine("O " + num + " e impar.");
+        }
+    }
+}
