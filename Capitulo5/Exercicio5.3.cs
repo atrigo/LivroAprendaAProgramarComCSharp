3eaf65f1526db8e714e92b29589def542c17d6ef
Antonio Trigo <antonio.trigo@gmail.com>
2018-01-22 11:20:17 +0000

    Update Exercicio5.3.cs

--- Capitulo5/Exercicio5.3.cs
+++ Capitulo5/Exercicio5.3.cs
@@ -4,3 +4,22 @@
  * Autores: Antonio Trigo e Jorge Henriques
  * Disponível em: http://www.silabo.pt
 */
+
+using System;
+
+namespace Cap5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i;
+            Console.Write("Introduza um número inteiro: "); // Não é feita validação, ou seja, o utilizador pode introduzir um nº real ou caracteres inválidos
+            i = Convert.ToInt32(Console.ReadLine()); 
+            if (i>0 && i%2==0)   // a segunda parte da expressão (i%2==0) só é avaliada se da primeira resultar o valor true 
+                Console.WriteLine("O número inteiro {0} é positivo e par",i); 
+            else
+                Console.WriteLine("O número inteiro {0} não é positivo e par",i); 
+        }
+    }
+}
