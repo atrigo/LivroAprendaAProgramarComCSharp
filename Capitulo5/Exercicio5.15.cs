475cfbec036fd7b65167c0d75249d430261fa18a
Antonio Trigo <antonio.trigo@gmail.com>
2018-01-22 18:13:58 +0000

    Update Exercicio5.15.cs

--- Capitulo5/Exercicio5.15.cs
+++ Capitulo5/Exercicio5.15.cs
@@ -5,6 +5,7 @@
  * Disponível em: http://www.silabo.pt
 */
 
+//Versão 1
 using System;
 
 namespace Cap5
@@ -55,6 +56,104 @@ namespace Cap5
                                                             Console.WriteLine("Capricórnio");
                                                         else
                                                             Console.WriteLine("Data inválida");
+        }
+    }
+}
+
+
+//Versão 2
+
+using System;
+
+namespace Cap5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int dia, mês;
+            Console.Write("Introduza o dia de nascimento: ");   
+            dia = Convert.ToInt32(Console.ReadLine());       
+            Console.Write("Introduza o mês de nascimento: ");
+            mês = Convert.ToInt32(Console.ReadLine());
+            switch (mês)
+            {
+                case 1:
+                    if (dia < 20)
+                        Console.WriteLine("Capricórnio");
+                    else
+                        Console.WriteLine("Aquário");
+                    break;
+                case 2:
+                    if (dia < 19)
+                        Console.WriteLine("Aquário");
+                    else
+                        Console.WriteLine("Peixes");
+                    break;
+                case 3:
+                    if (dia < 21)
+                        Console.WriteLine("Peixes");
+                    else
+                        Console.WriteLine("Carneiro");
+                    break;
+                case 4:
+                    if (dia < 21)
+                        Console.WriteLine("Carneiro");
+                    else
+                        Console.WriteLine("Touro");
+                    break;
+                case 5:
+                    if (dia < 21)
+                        Console.WriteLine("Touro");
+                    else
+                        Console.WriteLine("Gémeos");
+                    break;
+                case 6:
+                    if (dia < 21)
+                        Console.WriteLine("Gémeos");
+                    else
+                        Console.WriteLine("Caranguejo");
+                    break;
+                case 7:
+                    if (dia < 22)
+                        Console.WriteLine("Caranguejo");
+                    else
+                        Console.WriteLine("Leão");
+                    break;
+                case 8:
+                    if (dia < 23)
+                        Console.WriteLine("Leão");
+                    else
+                        Console.WriteLine("Virgem");
+                    break;
+                case 9:
+                    if (dia < 23)
+                        Console.WriteLine("Virgem");
+                    else
+                        Console.WriteLine("Balança");
+                    break;
+                case 10:
+                    if (dia < 23)
+                        Console.WriteLine("Balança");
+                    else
+                        Console.WriteLine("Escorpião");
+                    break;
+                case 11:
+                    if (dia < 22)
+                        Console.WriteLine("Escorpião");
+                    else
+                        Console.WriteLine("Sagitário");
+                    break;
+                case 12:
+                    if (dia < 22)
+                        Console.WriteLine("Sagitário");
+                    else
+                        Console.WriteLine("Capricórnio");
+                    break;
+                default:
+                    Console.WriteLine("Data inválida");
+                    break;
+            }
         }
     }
 }
