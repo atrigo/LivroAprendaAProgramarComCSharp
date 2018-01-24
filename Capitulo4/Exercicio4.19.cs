8c473320a2a29a34f0dc872be022e36bf430ad12
Antonio Trigo <antonio.trigo@gmail.com>
2018-01-24 10:29:43 +0000

    Update Exercicio4.19.cs

--- Capitulo4/Exercicio4.19.cs
+++ Capitulo4/Exercicio4.19.cs
@@ -5,3 +5,19 @@
  * Autores: Antonio Trigo e Jorge Henriques
  * Disponível em: http://www.silabo.pt
 */
+
+using System;
+
+namespace Cap4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int idade;
+            Console.Write("Qual é a sua idade actual: "); // Não é feita validação, ou seja, o utilizador pode introduzir caracteres inválidos
+            idade = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Daqui por vinte anos terá {0} anos",idade+20); 
+        }
+    }
+}
