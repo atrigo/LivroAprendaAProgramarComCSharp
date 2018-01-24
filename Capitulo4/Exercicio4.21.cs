a3c518474018f487e67f29423e06f112e7f45f11
Antonio Trigo <antonio.trigo@gmail.com>
2018-01-24 10:30:50 +0000

    Update Exercicio4.21.cs

--- Capitulo4/Exercicio4.21.cs
+++ Capitulo4/Exercicio4.21.cs
@@ -4,3 +4,20 @@
  * Autores: Antonio Trigo e Jorge Henriques
  * Disponível em: http://www.silabo.pt
 */
+
+using System;
+
+namespace Cap4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double p,t;
+            Console.Write("Qual foi a despesa do 1º dia: "); // Não é feita validação, ou seja, o utilizador pode introduzir caracteres inválidos
+            p = Convert.ToSingle(Console.ReadLine());
+            t = p * (1 + 1.2 + 1.2 * 1.2 + 1.2 * 1.2 * 1.2) / 4;
+            Console.WriteLine("Média diária dos 4 dias: {0:F0} ",t); 
+        }
+    }
+}
