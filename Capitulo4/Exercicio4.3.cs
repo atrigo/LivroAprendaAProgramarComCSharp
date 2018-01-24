7c214f14bbed722f2083de51168a369b263325da
Antonio Trigo <antonio.trigo@gmail.com>
2018-01-24 10:20:38 +0000

    Update Exercicio4.3.cs

--- Capitulo4/Exercicio4.3.cs
+++ Capitulo4/Exercicio4.3.cs
@@ -5,3 +5,16 @@
  * Autores: Antonio Trigo e Jorge Henriques
  * Disponível em: http://www.silabo.pt
 */
+
+using System;
+
+namespace Cap4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("O resto da divisão inteira de 40 por 3 é:{0}",40%3); // % é o operador resto da divisão
+        }
+    }
+}
