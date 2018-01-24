2648efc0269eeb78eb96902a0e655acffd73a3ea
Antonio Trigo <antonio.trigo@gmail.com>
2018-01-24 10:21:31 +0000

    Update Exercicio4.5.cs

--- Capitulo4/Exercicio4.5.cs
+++ Capitulo4/Exercicio4.5.cs
@@ -5,3 +5,18 @@
  * Autores: Antonio Trigo e Jorge Henriques
  * Disponível em: http://www.silabo.pt
 */
+
+using System;
+
+namespace Cap4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("{0:F3}\t{1:F3}\t{2:F3}\t{3:F3}",4.68805,4.8,5.9964,5); // No IDE do Visual Studio utilize o ponto como separador decimal, na consola utilize a vírgula
+            Console.WriteLine("{0:F2}\t{1:F2}\t{2:F2}\t{3:F2}",4.68805,4.8,5.9964,5); // \t - tabulador
+            Console.WriteLine("{0:F1}\t{1:F1}\t{2:F1}\t{3:F1}",4.68805,4.8,5.9964,5);
+        }
+    }
+}
