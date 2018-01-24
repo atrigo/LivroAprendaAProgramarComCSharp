a213869a6b8c656b0410bb0c0161233c67c4e83f
Antonio Trigo <antonio.trigo@gmail.com>
2018-01-24 10:27:57 +0000

    Update Exercicio4.17.cs

--- Capitulo4/Exercicio4.17.cs
+++ Capitulo4/Exercicio4.17.cs
@@ -5,3 +5,22 @@
  * Autores: Antonio Trigo e Jorge Henriques
  * Disponível em: http://www.silabo.pt
 */
+
+using System;
+
+namespace Cap4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float b,t;
+            Console.Write("Introduza o preço base: "); 
+            b = Convert.ToSingle(Console.ReadLine());// Não é feita validação, ou seja, o utilizador pode introduzir caracteres inválidos
+            Console.Write("Introduza a taxa de IVA: "); // Deve ser introduzido como um nº real e não em percentagem
+            t = Convert.ToSingle(Console.ReadLine());// Não é feita validação, ou seja, o utilizador pode introduzir caracteres inválidos
+            Console.WriteLine("PVP={0:F2}EUR",b*(1+t)); 
+        }
+    }
+}
+
