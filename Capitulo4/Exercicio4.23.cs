343ec59fb4f87608d994a88f48337cd706e9f0a4
Antonio Trigo <antonio.trigo@gmail.com>
2018-01-24 10:32:08 +0000

    Update Exercicio4.23.cs

--- Capitulo4/Exercicio4.23.cs
+++ Capitulo4/Exercicio4.23.cs
@@ -4,3 +4,21 @@
  * Autores: Antonio Trigo e Jorge Henriques
  * Disponível em: http://www.silabo.pt
 */
+
+using System;
+
+namespace Cap4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a=4,b=5;
+            Console.WriteLine("a:{0}\tb:{1}",a,b); //faça execução passo a passo (F10) para ver a evolução do conteúdo das variáveis
+            a = a + b; // a=9 b=5
+            b = a - b; // a=9 b=4
+            a = a - b; // a=5 b=4
+            Console.WriteLine("a:{0}\tb:{1}",a,b);
+        }
+    }
+}
