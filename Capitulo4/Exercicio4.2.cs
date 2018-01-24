c70a2a164a01455585d3930f9ad4cf42269dfb85
Antonio Trigo <antonio.trigo@gmail.com>
2018-01-24 10:20:02 +0000

    Update Exercicio4.2.cs

--- Capitulo4/Exercicio4.2.cs
+++ Capitulo4/Exercicio4.2.cs
@@ -5,3 +5,18 @@
  * Autores: Antonio Trigo e Jorge Henriques
  * Disponível em: http://www.silabo.pt
 */
+
+using System;
+
+namespace Cap4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("5/2={0}",5/2); // divisão inteira porque quer o numerador quer o denominador são inteiros
+            Console.WriteLine("5/2={0}",5/2.0); // divisão real porque o denominador é um nº real
+            Console.WriteLine("5/2={0}",(float)5/2); // divisão real porque o numerador é um nº real, foi efectuado um type cast (conversão de tipo)
+        }
+    }
+}
