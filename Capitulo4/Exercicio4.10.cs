33c46219d88291de27a799e7bb5dfc18d6fdbe06
Antonio Trigo <antonio.trigo@gmail.com>
2018-01-24 10:24:09 +0000

    Update Exercicio4.10.cs

--- Capitulo4/Exercicio4.10.cs
+++ Capitulo4/Exercicio4.10.cs
@@ -5,3 +5,21 @@
  * Autores: Antonio Trigo e Jorge Henriques
  * Disponível em: http://www.silabo.pt
 */
+
+using System;
+
+namespace Cap4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nome,apelido;
+            Console.Write("Introduza o seu nome: "); 
+            nome = Console.ReadLine();
+            Console.Write("Introduza o seu apelido: "); 
+            apelido = Console.ReadLine();
+            Console.WriteLine("{0}, {1}",apelido, nome);
+        }
+    }
+}
