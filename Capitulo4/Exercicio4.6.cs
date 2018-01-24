8c94d7b16a471011325d33db9787f32da198e12b
Antonio Trigo <antonio.trigo@gmail.com>
2018-01-24 10:22:05 +0000

    Update Exercicio4.6.cs

--- Capitulo4/Exercicio4.6.cs
+++ Capitulo4/Exercicio4.6.cs
@@ -5,3 +5,17 @@
  * Autores: Antonio Trigo e Jorge Henriques
  * Disponível em: http://www.silabo.pt
 */
+
+
+using System;
+
+namespace Cap4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("{0:P1}\n{1:P1}\n{2:P1}\n{3:P1}",4.68805,4.8,5.9964,5);// \n - mudança de linha
+        }
+    }
+}
