142b2b22cecf1dbd77f1b986d0bbe98e5d9ff931
Antonio Trigo <antonio.trigo@gmail.com>
2018-01-24 10:27:33 +0000

    Update Exercicio4.16.cs

--- Capitulo4/Exercicio4.16.cs
+++ Capitulo4/Exercicio4.16.cs
@@ -5,3 +5,20 @@
  * Autores: Antonio Trigo e Jorge Henriques
  * Disponível em: http://www.silabo.pt
 */
+
+
+using System;
+
+namespace Cap4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float b;
+            Console.Write("Introduza o preço base: "); // Não é feita validação, ou seja, o utilizador pode introduzir caracteres inválidos
+            b = Convert.ToSingle(Console.ReadLine());
+            Console.WriteLine("PVP={0:F2}EUR",b*1.23); 
+        }
+    }
+}
