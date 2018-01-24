80286477cc2388657382e81b57b3ff5a5d90260c
Antonio Trigo <antonio.trigo@gmail.com>
2018-01-24 10:25:14 +0000

    Update Exercicio4.12.cs

--- Capitulo4/Exercicio4.12.cs
+++ Capitulo4/Exercicio4.12.cs
@@ -5,3 +5,20 @@
  * Autores: Antonio Trigo e Jorge Henriques
  * Disponível em: http://www.silabo.pt
 */
+
+using System;
+
+namespace Cap4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float lado;
+            Console.Write("Introduza o valor do lado do quadrado: "); // Não é feita validação, ou seja, o utilizador pode introduzir caracteres inválidos
+            lado = Convert.ToSingle(Console.ReadLine());
+            Console.WriteLine("Área:{0}\nPerímetro:{1}",lado*lado,4*lado); // \n - mudança de linha
+        }
+    }
+}
+
