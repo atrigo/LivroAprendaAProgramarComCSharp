187cf9553986d7081ae100ae1454d518be145be5
Antonio Trigo <antonio.trigo@gmail.com>
2018-01-24 10:24:36 +0000

    Update Exercicio4.11.cs

--- Capitulo4/Exercicio4.11.cs
+++ Capitulo4/Exercicio4.11.cs
@@ -5,3 +5,22 @@
  * Autores: Antonio Trigo e Jorge Henriques
  * Disponível em: http://www.silabo.pt
 */
+
+
+using System;
+
+namespace Cap4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float a,b;
+            Console.Write("Introduza um número: "); // Não é feita validação, ou seja, o utilizador pode introduzir caracteres inválidos
+            a = Convert.ToSingle(Console.ReadLine());
+            Console.Write("Introduza um número: "); 
+            b = Convert.ToSingle(Console.ReadLine());
+            Console.WriteLine("{0}+{1}={2}",a,b,a+b); 
+        }
+    }
+}
