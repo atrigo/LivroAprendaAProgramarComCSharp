ca69f50c89d836e91eecaeb11d3d30e0c9c5406f
Antonio Trigo <antonio.trigo@gmail.com>
2018-01-24 10:21:07 +0000

    Update Exercicio4.4.cs

--- Capitulo4/Exercicio4.4.cs
+++ Capitulo4/Exercicio4.4.cs
@@ -5,3 +5,19 @@
  * Autores: Antonio Trigo e Jorge Henriques
  * Disponível em: http://www.silabo.pt
 */
+
+using System;
+
+namespace Cap4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float r;
+            Console.Write("Introduza um número real: "); // Não é feita validação, ou seja, o utilizador pode introduzir caracteres inválidos
+            r = Convert.ToSingle(Console.ReadLine());// Tenha em atenção que numa consola de Windows em Português o separador decimal é a vírgula
+            Console.WriteLine("A parte inteira de {0} é {1}",r,(int)r); // utilizou-se uma conversão de tipo (type cast)
+        }
+    }
+}
