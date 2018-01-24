300a1448e532fbeb45da960153d3ab3ad1518e8d
Antonio Trigo <antonio.trigo@gmail.com>
2018-01-24 10:32:58 +0000

    Update Exercicio4.24.cs

--- Capitulo4/Exercicio4.24.cs
+++ Capitulo4/Exercicio4.24.cs
@@ -4,3 +4,20 @@
  * Autores: Antonio Trigo e Jorge Henriques
  * Disponível em: http://www.silabo.pt
 */
+
+
+using System;
+
+namespace Cap4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            Console.Write("Introduza um nº inteiro: "); // Não é feita validação, ou seja, o utilizador pode introduzir caracteres inválidos
+            n = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("{0} {1}",n-1,n+1); 
+        }
+    }
+}
