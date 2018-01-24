2a2f2cc8578298bbfb1b85d8557f01c4f0f4a8dd
Antonio Trigo <antonio.trigo@gmail.com>
2018-01-24 10:26:42 +0000

    Update Exercicio4.14.cs

--- Capitulo4/Exercicio4.14.cs
+++ Capitulo4/Exercicio4.14.cs
@@ -5,3 +5,19 @@
  * Autores: Antonio Trigo e Jorge Henriques
  * Disponível em: http://www.silabo.pt
 */
+
+using System;
+
+namespace Cap4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float d,t=1.22595F;// Taxa de câmbio: 1,22595 - a letra F no fim do número significa que é do tipo float, sem o F seria considerado do tipo double e originaria um erro na atribuição
+            Console.Write("Quantos dólares pretende converter: "); // Não é feita validação, ou seja, o utilizador pode introduzir caracteres inválidos
+            d = Convert.ToSingle(Console.ReadLine());
+            Console.WriteLine("${0}={1}EUR",d,d*t); 
+        }
+    }
+}
