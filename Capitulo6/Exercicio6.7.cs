3f6409ed8a2974c27dba6a16f752649f47b9d592
Antonio Trigo <antonio.trigo@gmail.com>
2018-01-26 19:55:28 +0000

    Update Exercicio6.7.cs

--- Capitulo6/Exercicio6.7.cs
+++ Capitulo6/Exercicio6.7.cs
@@ -4,3 +4,33 @@
  * Autores: Antonio Trigo e Jorge Henriques
  * Disponível em: http://www.silabo.pt
 */
+
+using System;
+
+namespace Cap6
+{
+    class Program
+    {
+        static void Main(string[] args) 
+        {
+            int n, max, min;
+            Console.Write("Introduza um nº inteiro (zero para sair): ");
+            n = Convert.ToInt32(Console.ReadLine());
+            max = n;
+            min = n;
+            while (n != 0)
+            {
+                if (n > max)
+                    max = n;
+                if (n < min)
+                    min = n;
+                Console.Write("Introduza um nº inteiro (zero para sair): ");
+                n = Convert.ToInt32(Console.ReadLine());
+            } 
+            if(max==0 && min==0) // Se max e min têm o valor zero quer dizer que o primeiro nº digitado foi zero, ou seja, a condição de saída
+                Console.WriteLine("Não foi introduzido nenhum número"); 
+            else
+                Console.WriteLine("Máximo: {0}\nMínimo: {1}",max,min); 
+        }
+    }
+}
