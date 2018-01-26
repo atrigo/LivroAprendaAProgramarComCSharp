4405cb0718e5121fb27a457e007ee07d72783423
Antonio Trigo <antonio.trigo@gmail.com>
2018-01-26 20:06:03 +0000

    Update Exercicio6.19.cs

--- Capitulo6/Exercicio6.19.cs
+++ Capitulo6/Exercicio6.19.cs
@@ -4,3 +4,37 @@
  * Autores: Antonio Trigo e Jorge Henriques
  * Disponível em: http://www.silabo.pt
 */
+
+using System;
+
+namespace Cap6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N, n, i;
+            Console.Write("Altura da árvore:");
+            N = Convert.ToInt32(Console.ReadLine());
+            for (n = 0; n < N; n++) // Copa
+            {
+                for(i=0;i<N-n;i++)
+                    Console.Write(" ");
+                for(i=0;i<2*n+1;i++)
+                    Console.Write("*");
+                for(i=0;i<N-n;i++)
+                    Console.Write(" ");
+                Console.WriteLine();
+            }
+            for (n = 0; n < N/3; n++) // Tronco (1/3 da copa)
+            {
+                for (i = 0; i < 2 * N; i++)
+                    if(i==N)
+                        Console.Write("*");
+                    else
+                        Console.Write(" ");
+                Console.WriteLine();
+            }
+        }
+    }
+}
