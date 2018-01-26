75d1d04ec9375371eeadd6d77cfcc3509dc3b0a9
Antonio Trigo <antonio.trigo@gmail.com>
2018-01-26 20:04:48 +0000

    Update Exercicio6.16.cs

--- Capitulo6/Exercicio6.16.cs
+++ Capitulo6/Exercicio6.16.cs
@@ -4,3 +4,27 @@
  * Autores: Antonio Trigo e Jorge Henriques
  * Disponível em: http://www.silabo.pt
 */
+
+
+using System;
+
+namespace Cap6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N, n;
+            double soma = 0;
+            Console.Write("Quantas noites:");
+            N = Convert.ToInt32(Console.ReadLine());
+            Console.Write("S=");
+            for (n = 1; n < N; n++)
+            {
+                soma += 50F / n;
+                Console.Write("{0}/{1}+", 50, n);
+            }
+            Console.WriteLine("{0}/{1}\nSoma={2}", 50, n, soma += 50F / n);
+        }
+    }
+}
