e1b2e14cd39953ce6beda821a15a42c036cbf526
Antonio Trigo <antonio.trigo@gmail.com>
2018-01-26 19:53:53 +0000

    Update Exercicio6.5.cs

--- Capitulo6/Exercicio6.5.cs
+++ Capitulo6/Exercicio6.5.cs
@@ -4,3 +4,28 @@
  * Autores: Antonio Trigo e Jorge Henriques
  * Disponível em: http://www.silabo.pt
 */
+
+using System;
+
+namespace Cap6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int inf, sup, i, soma=0;
+            Console.Write("Introduza o limite inferior do intervalo: ");     // Não é feita validação, ou seja, o utilizador pode  introduzir 
+            inf = Convert.ToInt32(Console.ReadLine());                      //valores incorrectos ou caracteres inválidos e  como tal gerar excepções (ERROS)
+            Console.Write("Introduza o limite superior do intervalo: ");
+            sup = Convert.ToInt32(Console.ReadLine());
+            for (i = inf; i <= sup; i++) // Ciclo com i a variar entre inf e sup
+            {
+                Console.Write("{0}",i);
+                if(i!=sup)      // Se não for o último número acrescenta o operador +
+                    Console.Write("+"); 
+                soma += i;      //Vai adicionando cada um dos números à variável soma
+            }
+            Console.Write("={0}\n",soma); 
+        }
+    }
+}
