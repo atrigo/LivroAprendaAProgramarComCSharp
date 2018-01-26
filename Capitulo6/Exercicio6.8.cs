b2d1e0ce237eda8a17f8a9febdb9a6be0c0bcbfa
Antonio Trigo <antonio.trigo@gmail.com>
2018-01-26 19:55:49 +0000

    Update Exercicio6.8.cs

--- Capitulo6/Exercicio6.8.cs
+++ Capitulo6/Exercicio6.8.cs
@@ -4,3 +4,25 @@
  * Autores: Antonio Trigo e Jorge Henriques
  * Disponível em: http://www.silabo.pt
 */
+
+using System;
+
+namespace Cap6
+{
+    class Program
+    {
+        static void Main(string[] args) 
+        {
+            int n, soma=0;
+            do
+            {
+                Console.Write("Introduza um nº inteiro (zero para sair): ");
+                n = Convert.ToInt32(Console.ReadLine());
+                if (n > 0)
+                    soma += n;
+            } while (n != 0); 
+            Console.WriteLine("Soma dos inteiros positivos: {0}",soma); 
+        }
+    }
+}
+
