70adc4fd5986f6e71ce2b88c2313188ada443c11
Antonio Trigo <antonio.trigo@gmail.com>
2018-01-26 20:07:16 +0000

    Update Exercicio6.22.cs

--- Capitulo6/Exercicio6.22.cs
+++ Capitulo6/Exercicio6.22.cs
@@ -4,3 +4,36 @@
  * Autores: Antonio Trigo e Jorge Henriques
  * Disponível em: http://www.silabo.pt
 */
+
+using System;
+
+namespace Cap6
+{
+    class Program
+    {
+        static void Main(string[] args) 
+        {
+            int n, idade, i, soma=0;
+            Console.Write("Quantos alunos tem a turma: ");
+            n = Convert.ToInt32(Console.ReadLine());
+            if (n > 0)
+            {
+                for (i = 1; i < n; i++)
+                {
+                    Console.Write("Introduza a idade do aluno: ");
+                    idade = Convert.ToInt32(Console.ReadLine());
+                    soma += idade;
+                }
+                if((float)soma/n <26)
+                    Console.WriteLine("A turma é jovem"); 
+                else
+                    if((float)soma/n <=60)
+                        Console.WriteLine("A turma é adulta"); 
+                    else
+                        Console.WriteLine("A turma é idosa"); 
+            }
+            else
+                Console.WriteLine("A turma não tem alunos"); 
+        }
+    }
+}
