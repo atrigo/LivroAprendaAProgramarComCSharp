a55842c2eb19792e076163b82782dac2d010d99d
Antonio Trigo <antonio.trigo@gmail.com>
2018-01-26 20:16:25 +0000

    Update Exercicio6.38.cs

--- Capitulo6/Exercicio6.38.cs
+++ Capitulo6/Exercicio6.38.cs
@@ -4,3 +4,27 @@
  * Autores: Antonio Trigo e Jorge Henriques
  * Disponível em: http://www.silabo.pt
 */
+
+using System;
+
+namespace Cap6
+{
+    class Program
+    {
+        static void Main(string[] args) 
+        {
+            long i = 1, num = 416, n, r;
+            do
+            {
+                n = num * ++i;
+                do
+                {
+                    r = n % 10;
+                    n /= 10;
+                } while (n != 0 && (r == 1 || r == 2));
+            }
+            while (n != 0 || r != 1 && r != 2);
+            Console.WriteLine(num * i);
+        }
+    }
+}
