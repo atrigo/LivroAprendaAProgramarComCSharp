b6ec2f41f68a95f54cfb06d6ad9869ee90d63996
Antonio Trigo <antonio.trigo@gmail.com>
2018-01-26 20:02:58 +0000

    Update Exercicio6.12.cs

--- Capitulo6/Exercicio6.12.cs
+++ Capitulo6/Exercicio6.12.cs
@@ -4,3 +4,24 @@
  * Autores: Antonio Trigo e Jorge Henriques
  * Disponível em: http://www.silabo.pt
 */
+
+using System;
+
+namespace Cap6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i,j,num,r=0;
+            Console.Write("Digite um nº:");
+            num=Convert.ToInt32(Console.ReadLine());
+            for (i = 1; i <= 5; i++)
+            {
+                for(j=1;j<=5;j++)
+                    Console.Write("{0,5} ",r+=num);
+                Console.WriteLine();
+            }
+        }
+    }
+}
