87ccd436afeb41aa13905a738e1bad929f89922f
Antonio Trigo <antonio.trigo@gmail.com>
2018-01-26 20:17:30 +0000

    Update Exercicio6.41.cs

--- Capitulo6/Exercicio6.41.cs
+++ Capitulo6/Exercicio6.41.cs
@@ -4,3 +4,31 @@
  * Autores: Antonio Trigo e Jorge Henriques
  * Disponível em: http://www.silabo.pt
 */
+
+
+using System;
+
+namespace Cap6
+{
+    class Program
+    {
+        static void Main(string[] args) 
+        {
+            int n, i,j,soma=0; 
+            for (j = 0; j < 15; j++)
+            {
+                do
+                {
+                    Console.Write("Introduza um nº inteiro entre 15 e 50: ");
+                    n = Convert.ToInt32(Console.ReadLine());
+                } while (n < 15 || n > 50);
+                for (i = 2; i <= Math.Sqrt(n); i++)
+                    if (n % i == 0)
+                        break;
+                if (i > Math.Sqrt(n))
+                    soma+=n;
+            }
+            Console.WriteLine("Soma: {0}",soma);
+        }
+    }
+}
