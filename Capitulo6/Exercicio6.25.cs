467d665d56bab9091505894efd54db3cb1d13415
Antonio Trigo <antonio.trigo@gmail.com>
2018-01-26 20:08:19 +0000

    Update Exercicio6.25.cs

--- Capitulo6/Exercicio6.25.cs
+++ Capitulo6/Exercicio6.25.cs
@@ -4,3 +4,28 @@
  * Autores: Antonio Trigo e Jorge Henriques
  * Disponível em: http://www.silabo.pt
 */
+
+using System;
+
+namespace Cap6
+{
+    class Program
+    {
+        static void Main(string[] args) 
+        {
+            int n, num, soma=0;
+            Console.Write("Quantas turmas? ");
+            n = Convert.ToInt32(Console.ReadLine());
+            for(int i=1; i <= n; i++)
+            {
+                do
+                {
+                    Console.Write("Quantos alunos tem a turma {0}? ",i);
+                    num = Convert.ToInt32(Console.ReadLine());
+                } while (num<1 || num > 30);// o nº de alunos por turma deve estar entre 1 e 30
+                soma += num;
+            }
+            Console.WriteLine("Média de alunos por turma: {0}",soma/(float)n); // O type cast (float)n garante que a divisão é real
+        }
+    }
+}
