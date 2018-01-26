c269596e34e5be3cc9875886fd4d3fce050101d1
Antonio Trigo <antonio.trigo@gmail.com>
2018-01-26 20:07:36 +0000

    Update Exercicio6.23.cs

--- Capitulo6/Exercicio6.23.cs
+++ Capitulo6/Exercicio6.23.cs
@@ -4,3 +4,23 @@
  * Autores: Antonio Trigo e Jorge Henriques
  * Disponível em: http://www.silabo.pt
 */
+
+using System;
+
+namespace Cap6
+{
+    class Program
+    {
+        static void Main(string[] args) 
+        {
+            double A=80000, tcA=0.03,B=200000,tcB=0.015,a=0;
+            while (A < B)
+            {
+                A *= (1 + tcA);
+                B *= (1 + tcB);
+                a++;
+            }
+            Console.WriteLine("A população do país A igualará ou ultrapassará a população do país B em {0} anos",a);
+        }
+    }
+}
