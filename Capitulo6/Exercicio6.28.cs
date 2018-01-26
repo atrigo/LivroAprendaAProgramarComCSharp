0538f51471ef8748913a682bf009ea4e11ea3d3a
Antonio Trigo <antonio.trigo@gmail.com>
2018-01-26 20:10:00 +0000

    Update Exercicio6.28.cs

--- Capitulo6/Exercicio6.28.cs
+++ Capitulo6/Exercicio6.28.cs
@@ -4,3 +4,25 @@
  * Autores: Antonio Trigo e Jorge Henriques
  * Disponível em: http://www.silabo.pt
 */
+
+
+using System;
+
+namespace Cap6
+{
+    class Program
+    {
+        static void Main(string[] args) 
+        {
+            int n, soma = 0, i = 0;
+            Console.Write("Introduza um número binário? "); // Não é feita validação, ou seja, o utilizador pode introduzir caracteres inválidos e gerar excepções (ERROS)
+            n = Convert.ToInt32(Console.ReadLine());
+            do
+            {
+                soma += n % 10 * (int)Math.Pow(2, i++);
+                n = n / 10;
+            } while (n > 0);
+            Console.WriteLine("Decimal: {0}",soma);
+        }
+    }
+}
