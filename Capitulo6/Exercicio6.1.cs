c069f3a6b0326231c6a10a0d7ab7f927304cc40e
Antonio Trigo <antonio.trigo@gmail.com>
2018-01-26 19:51:22 +0000

    Update Exercicio6.1.cs

--- Capitulo6/Exercicio6.1.cs
+++ Capitulo6/Exercicio6.1.cs
@@ -4,3 +4,19 @@
  * Autores: Antonio Trigo e Jorge Henriques
  * Disponível em: http://www.silabo.pt
 */
+
+
+using System;
+
+namespace Cap6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i;
+            for(i=1;i<=50;i+=2)
+                Console.WriteLine(i);
+        }
+    }
+}
