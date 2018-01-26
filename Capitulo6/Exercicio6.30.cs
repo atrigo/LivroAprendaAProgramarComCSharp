d052a11326297a75eb1c2d1f0b7e4651302553e0
Antonio Trigo <antonio.trigo@gmail.com>
2018-01-26 20:10:44 +0000

    Update Exercicio6.30.cs

--- Capitulo6/Exercicio6.30.cs
+++ Capitulo6/Exercicio6.30.cs
@@ -4,3 +4,30 @@
  * Autores: Antonio Trigo e Jorge Henriques
  * Disponível em: http://www.silabo.pt
 */
+
+
+using System;
+
+namespace Cap6
+{
+    class Program
+    {
+        static void Main(string[] args) 
+        {
+            int num = 0, r, d, n;
+            do
+            {
+                n = ++num;
+                d = n % 10;
+                n = n / 10;
+                r = n;
+                while (n != 0)
+                {
+                    n = n / 10;
+                    d *= 10;
+                }
+            } while (5 * num != d + r);
+            Console.WriteLine("{0}", num);
+        }
+    }
+}
