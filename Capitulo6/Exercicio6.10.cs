ad7809d25571a467f0e03f42bf3f42887c10e3b9
Antonio Trigo <antonio.trigo@gmail.com>
2018-01-26 19:56:45 +0000

    Update Exercicio6.10.cs

--- Capitulo6/Exercicio6.10.cs
+++ Capitulo6/Exercicio6.10.cs
@@ -4,3 +4,25 @@
  * Autores: Antonio Trigo e Jorge Henriques
  * Disponível em: http://www.silabo.pt
 */
+
+
+using System;
+
+namespace Cap6
+{
+    class Program
+    {
+        static void Main(string[] args) 
+        {
+            int n,soma=0;
+            Console.Write("Introudza um número inteiro: ");
+            n = Convert.ToInt32(Console.ReadLine());
+            while (n != 0) // Sugestão: Faça execução passo a passo (F10) e observe os valores das variáveis na janela Locais
+            {
+                soma += n % 10; // O resto da divisão por 10 permite obter cada um dos dígitos do número
+                n /= 10; // Depois da obtenção esse dígito é descartado
+            } 
+            Console.WriteLine("Soma dos dígitos:{0}", soma);
+        }
+    }
+}
