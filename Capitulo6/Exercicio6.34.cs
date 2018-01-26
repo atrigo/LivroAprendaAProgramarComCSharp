d1965dedbcc7ef768bd37fc46763bd6bfd1b8041
Antonio Trigo <antonio.trigo@gmail.com>
2018-01-26 20:15:05 +0000

    Update Exercicio6.34.cs

--- Capitulo6/Exercicio6.34.cs
+++ Capitulo6/Exercicio6.34.cs
@@ -4,3 +4,31 @@
  * Autores: Antonio Trigo e Jorge Henriques
  * Disponível em: http://www.silabo.pt
 */
+
+using System;
+
+namespace Cap6
+{
+    class Program
+    {
+        static void Main(string[] args) 
+        {
+            ulong j, m = 2, n, k = 0;
+            bool primo = true;
+            for (n = 2; n < 62; n++) // Dependendo do computador o último número pode demorar mais de 1 minuto a ser calculado
+            {
+                m = m * 2 - 1;
+                primo = true;
+                j = 1;
+                while (++j <= (Math.Sqrt(m)) && primo)
+                    if (m % j == 0)
+                        primo = false;
+                if (primo)
+                    Console.WriteLine(m);
+                k += j - 2;
+                m++;
+            }
+            Console.WriteLine("\n\n\t ****** {0} Iterações ******\n\n", k);
+        }
+    }
+}
