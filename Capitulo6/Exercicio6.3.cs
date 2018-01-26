e40c44dfcb877f838ed42d538a2c2509421fa49b
Antonio Trigo <antonio.trigo@gmail.com>
2018-01-26 19:53:01 +0000

    Update Exercicio6.3.cs

--- Capitulo6/Exercicio6.3.cs
+++ Capitulo6/Exercicio6.3.cs
@@ -4,3 +4,36 @@
  * Autores: Antonio Trigo e Jorge Henriques
  * Disponível em: http://www.silabo.pt
 */
+
+using System;
+
+namespace Cap6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i,soma;
+            soma = 0;
+            for (i = 4; i < 100; i+=3)
+                soma += i;
+            Console.WriteLine("Soma:{0}",soma);
+            soma = 0; //Após a execução do ciclo for é necessário repor os valores das variáveis soma e i
+            i = 4;
+            while(i < 100)
+            {
+                soma += i;
+                i += 3;
+            }
+            Console.WriteLine("Soma:{0}",soma);
+            soma = 0;//Após a execução do ciclo while é necessário repor os valores das variáveis soma e i
+            i = 4;
+            do
+            {
+                soma += i;
+                i += 3;
+            } while (i < 100);
+            Console.WriteLine("Soma:{0}",soma);
+        }
+    }
+}
