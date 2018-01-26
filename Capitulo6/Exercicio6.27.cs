3dd62fa2273afd7be71a224bd070112323962ce4
Antonio Trigo <antonio.trigo@gmail.com>
2018-01-26 20:09:38 +0000

    Update Exercicio6.27.cs

--- Capitulo6/Exercicio6.27.cs
+++ Capitulo6/Exercicio6.27.cs
@@ -4,3 +4,25 @@
  * Autores: Antonio Trigo e Jorge Henriques
  * Disponível em: http://www.silabo.pt
 */
+
+
+using System;
+
+namespace Cap6
+{
+    class Program
+    {
+        static void Main(string[] args) 
+        {
+            int n, soma = 0, i = 0;
+            Console.Write("Introduza um número decimal? ");
+            n = Convert.ToInt32(Console.ReadLine());
+            do
+            {
+                soma += n % 2 * (int)Math.Pow(10, i++);
+                n = n / 2;
+            } while (n > 0);
+            Console.WriteLine("Binário: {0}",soma);
+        }
+    }
+}
