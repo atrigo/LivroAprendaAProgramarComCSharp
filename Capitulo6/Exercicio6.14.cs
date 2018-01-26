3538a7f0c62ef5da5e28b82f8edd65561bf6a1ff
Antonio Trigo <antonio.trigo@gmail.com>
2018-01-26 20:04:02 +0000

    Update Exercicio6.14.cs

--- Capitulo6/Exercicio6.14.cs
+++ Capitulo6/Exercicio6.14.cs
@@ -4,3 +4,28 @@
  * Autores: Antonio Trigo e Jorge Henriques
  * Disponível em: http://www.silabo.pt
 */
+
+using System;
+
+namespace Cap6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N, n = 1, m = 1;
+            float soma = 0;
+            Console.Write("Quantos termos? ");
+            N = Convert.ToInt32(Console.ReadLine());
+            Console.Write("S=");
+            do
+            {
+                soma += (float)n / m; // ou soma=soma + (float)n/m
+                Console.Write("{0}/{1}+", n, m);
+                n++;
+                m = m + 2;// ou m+=2
+            } while (n < N);
+            Console.WriteLine("{0}/{1}\nsoma={2}", n, m, soma += (float)n / m);
+        }
+    }
+}
