38f03becb7c6bfe50b2c98df4871002c46de84ef
Antonio Trigo <antonio.trigo@gmail.com>
2018-01-26 20:10:20 +0000

    Update Exercicio6.29.cs

--- Capitulo6/Exercicio6.29.cs
+++ Capitulo6/Exercicio6.29.cs
@@ -4,3 +4,28 @@
  * Autores: Antonio Trigo e Jorge Henriques
  * Disponível em: http://www.silabo.pt
 */
+
+using System;
+
+namespace Cap6
+{
+    class Program
+    {
+        static void Main(string[] args) 
+        {
+            int n, soma = 0;
+            Console.Write("Quantas entradas para menores de 4 anos? ");
+            n = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Quantas entradas para crianças entre os 6 e os 12 anos? ");
+            n = Convert.ToInt32(Console.ReadLine());
+            soma += 6 * n;
+            Console.Write("Quantas entradas para crianças entre os 12 e os 17 anos? ");
+            n = Convert.ToInt32(Console.ReadLine());
+            soma += 12 * n;
+            Console.Write("Quantas entradas para adultos? ");
+            n = Convert.ToInt32(Console.ReadLine());
+            soma += 18 * n;
+            Console.WriteLine("Total: {0}",soma);
+        }
+    }
+}
