1ee49ea3af2571ae7918e83d2feeb78ffd5af3b5
Antonio Trigo <antonio.trigo@gmail.com>
2018-01-26 20:05:40 +0000

    Update Exercicio6.18.cs

--- Capitulo6/Exercicio6.18.cs
+++ Capitulo6/Exercicio6.18.cs
@@ -4,3 +4,30 @@
  * Autores: Antonio Trigo e Jorge Henriques
  * Disponível em: http://www.silabo.pt
 */
+
+using System;
+
+namespace Cap6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N, n, i,cont=0;
+            Console.Write("Introduza o valor de N:");
+            N = Convert.ToInt32(Console.ReadLine());
+            for (n = 2; n <= N; n++)
+            {
+                for (i = 2; i <= Math.Sqrt(n); i++)
+                {
+                    cont++;
+                    if (n % i == 0)
+                        break;
+                }
+                if(i > Math.Sqrt(n))
+                    Console.Write(" {0}",n);
+            }
+            Console.WriteLine("\nForam efectuadas {0} divisões", cont);
+        }
+    }
+}
