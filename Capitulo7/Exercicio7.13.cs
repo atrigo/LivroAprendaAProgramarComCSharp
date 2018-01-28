30cad3e469234e6a701347cd8baad64f79682a02
Antonio Trigo <antonio.trigo@gmail.com>
2018-01-28 16:34:53 +0000

    Update Exercicio7.13.cs

--- Capitulo7/Exercicio7.13.cs
+++ Capitulo7/Exercicio7.13.cs
@@ -4,3 +4,31 @@
  * Autores: Antonio Trigo e Jorge Henriques
  * Disponível em: http://www.silabo.pt
 */
+
+using System;
+
+namespace Cap7
+{
+    class Program
+    {
+        static int dado()
+        {
+            return rnd.Next(1, 7);
+        }
+        static Random rnd = new Random();
+
+        static void Main(string[] args)
+        {
+            int N, v, contador = 0;
+            Console.Write("Quantos lançamentos do dado: ");
+            N = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i < N; i++)
+            {
+                if ((v = dado()) == 6)
+                    contador++;
+                Console.WriteLine(v);
+            }
+            Console.WriteLine("O número 6 ocorreu {0} vezes", contador);
+        }
+    }
+}
