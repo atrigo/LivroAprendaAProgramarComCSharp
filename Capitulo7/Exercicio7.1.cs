2fbe944a8dd81c23f39db30114c7389cd1c16efe
Antonio Trigo <antonio.trigo@gmail.com>
2018-01-28 16:26:09 +0000

    Update Exercicio7.1.cs

--- Capitulo7/Exercicio7.1.cs
+++ Capitulo7/Exercicio7.1.cs
@@ -4,3 +4,22 @@
  * Autores: Antonio Trigo e Jorge Henriques
  * Disponível em: http://www.silabo.pt
 */
+
+using System;
+
+namespace Cap7
+{
+    class Program
+    {
+        static void Sauda(string nome)
+        {
+            Console.WriteLine("Olá {0} ", nome);
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("Qual o nome? ");
+            string nome = Console.ReadLine();
+            Sauda(nome);
+        }
+    }
+}
