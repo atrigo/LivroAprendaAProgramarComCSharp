b5c3aa5fe79c2f6bac6dc2cc71b36ef15072fbb9
Antonio Trigo <antonio.trigo@gmail.com>
2018-01-28 16:31:32 +0000

    Update Exercicio7.9.cs

--- Capitulo7/Exercicio7.9.cs
+++ Capitulo7/Exercicio7.9.cs
@@ -4,3 +4,32 @@
  * Autores: Antonio Trigo e Jorge Henriques
  * Disponível em: http://www.silabo.pt
 */
+
+using System;
+
+namespace Cap7
+{
+    class Program
+    {
+        static bool Par(int n)
+        {
+            if (n % 2 == 0)
+                return true;
+            else
+                return false;
+        }
+
+        static void Main(string[] args)
+        {
+            int N,n=0,cont=0;
+            Console.Write("Quantos números pares? ");
+            N = Convert.ToInt32(Console.ReadLine());
+            while(cont < N)
+                if (Par(++n))
+                {
+                    Console.WriteLine(n);
+                    cont++;
+                }
+        }
+    }
+}
