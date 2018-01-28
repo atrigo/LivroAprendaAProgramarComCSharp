18ec05d57d94d8b86d429402506a1bb54e9e9391
Antonio Trigo <antonio.trigo@gmail.com>
2018-01-28 16:33:32 +0000

    Update Exercicio7.11.cs

--- Capitulo7/Exercicio7.11.cs
+++ Capitulo7/Exercicio7.11.cs
@@ -4,3 +4,29 @@
  * Autores: Antonio Trigo e Jorge Henriques
  * Disponível em: http://www.silabo.pt
 */
+
+using System;
+
+namespace Cap7
+{
+    class Program
+    {
+        static uint numDigitos(uint n)
+        {
+            uint contador = 0;
+            do
+            {
+                n = n / 10;
+                contador++;
+            } while (n != 0);
+            return contador;
+        }
+        static void Main(string[] args)
+        {
+            uint N;
+            Console.Write("Introduza um nº inteiro positivo: ");
+            N = Convert.ToUInt32(Console.ReadLine());
+            Console.WriteLine("O número {0} tem {1} dígitos", N, numDigitos(N));
+        }
+    }
+}
