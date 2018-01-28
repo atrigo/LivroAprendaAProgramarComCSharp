f70cb66e20581060fa964d362073219386a0f0be
Antonio Trigo <antonio.trigo@gmail.com>
2018-01-28 16:26:45 +0000

    Update Exercicio7.2.cs

--- Capitulo7/Exercicio7.2.cs
+++ Capitulo7/Exercicio7.2.cs
@@ -4,3 +4,26 @@
  * Autores: Antonio Trigo e Jorge Henriques
  * Disponível em: http://www.silabo.pt
 */
+
+using System;
+
+namespace Cap7
+{
+    class Program
+    {
+        static int Soma(int a, int b)
+        {
+            return a+b;
+        }
+
+        static void Main(string[] args)
+        {
+            int x, y;
+            Console.Write("Introduza o 1º valor: ");
+            x = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Introduza o 2º valor: ");
+            y = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Soma: {0}", Soma(x, y));
+        }
+    }
+}
