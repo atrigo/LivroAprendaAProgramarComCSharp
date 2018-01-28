274ed8bc475cffb73c4d51d5bd70c0bf663bb537
Antonio Trigo <antonio.trigo@gmail.com>
2018-01-28 16:27:21 +0000

    Update Exercicio7.3.cs

--- Capitulo7/Exercicio7.3.cs
+++ Capitulo7/Exercicio7.3.cs
@@ -4,3 +4,25 @@
  * Autores: Antonio Trigo e Jorge Henriques
  * Disponível em: http://www.silabo.pt
 */
+
+
+namespace Cap7
+{
+    class Program
+    {
+        static void linhaAster(int N)
+        {
+            for(int i=0;i<N;i++)
+                Console.Write("*");
+            Console.WriteLine();
+        }
+
+        static void Main(string[] args)
+        {
+            int N;
+            Console.Write("Quantos asteriscos? ");
+            N = Convert.ToInt32(Console.ReadLine());
+            linhaAster(N);
+        }
+    }
+}
