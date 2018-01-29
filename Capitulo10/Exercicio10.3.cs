bd6c8dbae936214b186e30b038f668ef9918fcb2
Antonio Trigo <antonio.trigo@gmail.com>
2018-01-29 16:06:18 +0000

    Update Exercicio10.3.cs

--- Capitulo10/Exercicio10.3.cs
+++ Capitulo10/Exercicio10.3.cs
@@ -4,3 +4,36 @@
  * Autores: Antonio Trigo e Jorge Henriques
  * Disponível em: http://www.silabo.pt
 */
+
+using System;
+
+namespace Cap10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num=0, N=4,soma=0;
+            bool continua;
+            for(int i=0; i<N; i++)
+            {
+                do
+                {
+                    try
+                    {
+                        Console.Write("Introduza um número inteiro: ");
+                        num = Convert.ToInt32(Console.ReadLine());
+                        continua = false;
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                        continua = true;
+                    }
+                } while (continua);
+                soma += num;
+            }
+            Console.WriteLine("Média: {0}",soma/(float)N);
+        }
+    }
+}
