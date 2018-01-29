1d3c789f5444cfadaa23e3875a423f2cb49b4857
Antonio Trigo <antonio.trigo@gmail.com>
2018-01-29 14:46:00 +0000

    Update Exercicio9.1.cs

--- Capitulo9/Exercicio9.1.cs
+++ Capitulo9/Exercicio9.1.cs
@@ -4,3 +4,19 @@
  * Autores: Antonio Trigo e Jorge Henriques
  * Disponível em: http://www.silabo.pt
 */
+
+using System;
+
+namespace Cap9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nome;
+            Console.Write("Introduza o seu nome: ");
+            nome = Console.ReadLine();
+            Console.WriteLine("O seu nome é {0} ", nome);
+        }
+    }
+}
