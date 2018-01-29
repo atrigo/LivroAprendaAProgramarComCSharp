ae552c26821669b485b428986ccf8c3637945f8a
Antonio Trigo <antonio.trigo@gmail.com>
2018-01-29 14:46:37 +0000

    Update Exercicio9.2.cs

--- Capitulo9/Exercicio9.2.cs
+++ Capitulo9/Exercicio9.2.cs
@@ -4,3 +4,21 @@
  * Autores: Antonio Trigo e Jorge Henriques
  * Disponível em: http://www.silabo.pt
 */
+
+using System;
+
+namespace Cap9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nome,apelido;
+            Console.Write("Introduza o seu nome próprio: ");
+            nome = Console.ReadLine();
+            Console.Write("Introduza o seu apelido: ");
+            apelido = Console.ReadLine();
+            Console.WriteLine("O seu nome completo é {0} {1}", nome,apelido);
+        }
+    }
+}
