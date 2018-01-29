f97c5176733df41b2dd5f969023de198dc825d8e
Antonio Trigo <antonio.trigo@gmail.com>
2018-01-29 16:03:12 +0000

    Update Exercicio10.1.cs

--- Capitulo10/Exercicio10.1.cs
+++ Capitulo10/Exercicio10.1.cs
@@ -4,3 +4,59 @@
  * Autores: Antonio Trigo e Jorge Henriques
  * Disponível em: http://www.silabo.pt
 */
+
+//Versao 1
+
+using System;
+
+namespace Cap10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            bool continua;
+            do
+            {
+                try
+                {
+                    Console.Write("Introduza um número inteiro: ");
+                    n = Convert.ToInt32(Console.ReadLine());
+                    continua = false;
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    continua = true;
+                }
+            } while (continua);
+
+        }
+    }
+}
+
+//Versao 2
+
+using System;
+
+namespace Cap9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+ VALIDA:    try
+            {
+                Console.Write("Introduza um número inteiro: ");
+                n = Convert.ToInt32(Console.ReadLine());
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                goto VALIDA;
+            }
+        }
+    }
+}
