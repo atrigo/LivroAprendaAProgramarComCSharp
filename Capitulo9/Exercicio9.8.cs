225f04e927c05facc5764708bfd8fee0385fb73e
Antonio Trigo <antonio.trigo@gmail.com>
2018-01-29 14:50:03 +0000

    Update Exercicio9.8.cs

--- Capitulo9/Exercicio9.8.cs
+++ Capitulo9/Exercicio9.8.cs
@@ -4,3 +4,31 @@
  * Autores: Antonio Trigo e Jorge Henriques
  * Disponível em: http://www.silabo.pt
 */
+
+using System;
+
+namespace Cap9
+{
+    class Program
+    {
+        static void insereAster(ref string s)
+        {
+            int i = 0;
+            char[] v=new char[s.Length*2];
+            for (int j = 0; j <s.Length ; j++)
+            {
+                v[i++] = s[j];
+                v[i++] = '*';
+            }
+            s = new string(v);
+        }
+        static void Main(string[] args)
+        {
+            string s;
+            Console.Write("Introduza uma frase: ");
+            s = Console.ReadLine();
+            insereAster(ref s); // Sem a utilização da passagem por referência as alterações efectuadas no método insereAster não se refectiriam na função main
+            Console.WriteLine(s);
+        }
+    }
+}
