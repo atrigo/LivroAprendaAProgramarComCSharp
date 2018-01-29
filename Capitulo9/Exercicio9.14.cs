5f4bbccdcd3912be0186db13e4d668979c3c7d14
Antonio Trigo <antonio.trigo@gmail.com>
2018-01-29 14:53:45 +0000

    Update Exercicio9.14.cs

--- Capitulo9/Exercicio9.14.cs
+++ Capitulo9/Exercicio9.14.cs
@@ -4,3 +4,30 @@
  * Autores: Antonio Trigo e Jorge Henriques
  * Disponível em: http://www.silabo.pt
 */
+
+using System;
+
+namespace Cap9
+{
+    class Program
+    {
+        static bool palindromo(string s)
+        {
+            for (int i = 0; i < s.Length / 2; i++)
+                if (s[i] != s[s.Length - 1 - i])
+                    return false;
+            return true;
+        }
+
+        static void Main(string[] args)
+        {
+            string s;
+            Console.Write("Introduza uma frase: ");
+            s = Console.ReadLine();
+            if(palindromo(s))
+                Console.WriteLine("É um palíndromo");
+            else
+                Console.WriteLine("Não é um palíndromo");
+        }
+    }
+}
