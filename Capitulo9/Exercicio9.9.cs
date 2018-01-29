914f775da949b7a851ac0da59165dbdac018bb4f
Antonio Trigo <antonio.trigo@gmail.com>
2018-01-29 14:50:23 +0000

    Update Exercicio9.9.cs

--- Capitulo9/Exercicio9.9.cs
+++ Capitulo9/Exercicio9.9.cs
@@ -4,3 +4,28 @@
  * Autores: Antonio Trigo e Jorge Henriques
  * Disponível em: http://www.silabo.pt
 */
+
+using System;
+
+namespace Cap9
+{
+    class Program
+    {
+        static void maiusculas(ref string s)
+        {
+            char[] v=new char[s.Length];
+            for (int j = 0; j <s.Length ; j++)
+                if(s[j]>='a'&&s[j]<='z') // Testa se o caráter é uma letra minúscula e portanto suscetível de ser convertido em maiúscula
+                    v[j] = (char)(s[j]-32);// A diferença do código ASCII de maiúsculas para minúsculas é 32 (A-65 a-97)
+            s = new string(v);
+        }
+        static void Main(string[] args)
+        {
+            string s;
+            Console.Write("Introduza uma frase: ");
+            s = Console.ReadLine();
+            maiusculas(ref s); // Sem a utilização da passagem por referência as alterações efectuadas no método insereAster não se refectiriam na função main
+            Console.WriteLine(s);
+        }
+    }
+}
