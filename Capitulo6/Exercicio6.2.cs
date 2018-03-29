f9ee2d568da42f0bc3d74b87e8ac08d5b5c8a79b
Antonio Trigo <antonio.trigo@gmail.com>
2018-03-29 11:06:52 +0100

    Update Exercicio6.2.cs

--- Capitulo6/Exercicio6.2.cs
+++ Capitulo6/Exercicio6.2.cs
@@ -1,10 +1,12 @@
 /*
- * Exercicio 6.2
+ * Exercicio 6.2. 
+ * Enunciado:  Escreva um programa que imprima no ecrã os números ímpares entre 1 e 50.
  * Livro: Aprenda a programar com C#
  * Autores: Antonio Trigo e Jorge Henriques
  * Disponível em: http://www.silabo.pt
 */
 
+//Versao 1
 using System;
 
 namespace Cap6
@@ -13,17 +15,28 @@ namespace Cap6
     {
         static void Main(string[] args)
         {
-            int i, j, l, c;
-            Console.Write("Introduza a largura: ");     // Não é feita validação, ou seja, o utilizador pode 
-            l = Convert.ToInt32(Console.ReadLine());    // introduzir caracteres inválidos e gerar excepções (ERROS)
-            Console.Write("Introduza o comprimento: ");
-            c = Convert.ToInt32(Console.ReadLine());
-            for (i = 0; i < l; i++) // largura do retângulo (nº de linhas = l)
-            {
-                for (j = 0; j < c; j++) // comprimento do retângulo (nº de colunas = c)
-                    Console.Write("X");
-                Console.WriteLine();    // muda de linha quando termina de escrever todas as colunas
-            }
+            int i;
+            for(i=1;i<=50;i+=2)
+                Console.WriteLine(i);
         }
     }
 }
+
+//Versao 2
+using System;
+
+namespace Cap6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i;
+            for(i=1;i<=50;i++)
+                if(i%2==1)
+                    Console.Write("{0} ",i);
+            Console.WriteLine();
+        }
+    }
+}
+
