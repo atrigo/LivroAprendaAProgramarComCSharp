88f6d331fc4f50a24456674fcc060df4a2d8c296
Antonio Trigo <antonio.trigo@gmail.com>
2018-03-29 11:29:10 +0100

    Update Exercicio6.19.cs

--- Capitulo6/Exercicio6.19.cs
+++ Capitulo6/Exercicio6.19.cs
@@ -1,5 +1,6 @@
 /*
  * Exercicio 6.19
+ * Enunciado: Escreva um programa que imprima uma árvore de ``*''.
  * Livro: Aprenda a programar com C#
  * Autores: Antonio Trigo e Jorge Henriques
  * Disponível em: http://www.silabo.pt
