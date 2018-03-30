bfc2c92d48a00c85c1cf5a3eb5651e7b4b3b52e3
Antonio Trigo <antonio.trigo@gmail.com>
2018-03-30 23:45:11 +0100

    Update Exercicio6.25.cs

--- Capitulo6/Exercicio6.25.cs
+++ Capitulo6/Exercicio6.25.cs
@@ -1,5 +1,8 @@
 /*
  * Exercicio 6.25
+ * Enunciado:   Escreva um programa que calcule o número médio de alunos por turma. 
+ *              Para isto, peça a quantidade de turmas e a quantidade de alunos de cada turma. 
+ *              As turmas não podem ter mais de 30 alunos.
  * Livro: Aprenda a programar com C#
  * Autores: Antonio Trigo e Jorge Henriques
  * Disponível em: http://www.silabo.pt
